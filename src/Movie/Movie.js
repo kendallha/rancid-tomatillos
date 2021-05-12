f1d8be5803a7a2798b1254123716aaf5ccb09351
Marceline Ball <marchettiball@gmail.com>
2021-05-12 15:33:35 -0600

    Add return Movie to MovieList component

---
+++ src/Movie/Movie.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Movie = () => {
+  return(
+    <article>
+  )
+}
+
+export default Movie
--- src/MovieList/MovieList.js
+++ src/MovieList/MovieList.js
@@ -1,6 +1,19 @@
 import React from 'react'
+import Movie from '../Movie/Movie'
 
-const MovieList = (props) => {
+const MovieList = ({ movies }) => {
+  const movieTiles = props.movies.map(movie => {
+    return (
+      <Movie
+        id={movie.id}
+        poster={movie.poster_path}
+        backdrop={movie.backdrop_path}
+        title={movie.title}
+        rating={movie.average_rating}
+        releaseDate={movie.release_date}
+      />
+    )
+  })
   return <p>Movies go here!</p>
 }
 
