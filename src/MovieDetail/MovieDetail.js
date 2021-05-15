4b4475fa6a0daa27c5d55f4eaad413be03a60432
kendallha <kendallhaw@gmail.com>
2021-05-14 18:55:40 -0700

    Add cursor hover states, remove commented out code, add ARIA role for icon per code review

--- src/MovieDetail/MovieDetail.js
+++ src/MovieDetail/MovieDetail.js
@@ -7,27 +7,30 @@ const MovieDetail = ( {movie, goBack} ) => {
         backgroundImage: 'url('+ movie.backdrop_path +')'
       }}>
       <article className='movie-expanded'>
-      <i onClick={() => goBack()} className="fas fa-arrow-left fa-2x"></i>
-        <div className='double-poster-wrapper'>
-          <div className='poster-wrapper'>
-            <img className='poster' src={movie.poster_path} />
-            <p className='average-rating'>{movie.average_rating.toFixed(1)}</p>
+      <i onClick={() => goBack()} 
+      className="fas fa-arrow-circle-left fa-3x"
+      role='button'>
+      </i>
+      <div className='double-poster-wrapper'>
+        <div className='poster-wrapper'>
+          <img className='poster' src={movie.poster_path} />
+          <p className='average-rating'>{movie.average_rating.toFixed(1)}</p>
+        </div>
+      </div>
+      <div className='info-section'>
+        <h3 className='movie-title'>{movie.title} ({movie.release_date.substring(0,4)})</h3>
+        <p className='tagline'>{movie.tagline}</p>
+        <div className='top-card'>
+          <div className='main-details'>
+            <p>{movie.genres}</p>
+            <p>{movie.runtime} minutes</p>
+          </div>
+          <div className='movie-details-aside'>
+            <p>Budget: ${movie.budget.toLocaleString()}</p>
+            <p>Revenue: ${movie.revenue.toLocaleString()}</p>
           </div>
         </div>
-        <div className='info-section'>
-          <h3 className='movie-title'>{movie.title} ({movie.release_date.substring(0,4)})</h3>
-          <p className='tagline'>{movie.tagline}</p>
-          <div className='top-card'>
-            <div className='main-details'>
-              <p>{movie.genres}</p>
-              <p>{movie.runtime} minutes</p>
-            </div>
-            <div className='movie-details-aside'>
-              <p>Budget: ${movie.budget.toLocaleString()}</p>
-              <p>Revenue: ${movie.revenue.toLocaleString()}</p>
-            </div>
-          </div>
-          <p>{movie.overview}</p>
+        <p>{movie.overview}</p>
         </div>
       </article>
     </div>
