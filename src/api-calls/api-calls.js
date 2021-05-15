af27f32a775cefaa259025e50f951363e32a6f37
Marceline Ball <marchettiball@gmail.com>
2021-05-15 11:33:04 -0600

    Add api call for maovie details

--- src/api-calls/api-calls.js
+++ src/api-calls/api-calls.js
@@ -1,4 +1,9 @@
 export const getAllMovies = () => {
-  return fetch('https://rancid-tomatillos.herokuapp.com/api/v2')
+  return fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
+    .then(response => response.json())
+}
+
+export const getMovieDetails = (id) => {
+  return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
     .then(response => response.json())
 }
