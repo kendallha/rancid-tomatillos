7f4a000b28aaa4b855f04250a96c96986899a33d
Marceline Ball <marchettiball@gmail.com>
2021-05-15 12:08:48 -0600

    Refactor App to take api data into state, and render api data

--- src/App/App.js
+++ src/App/App.js
@@ -1,32 +1,47 @@
 import React, { Component } from 'react';
 import MovieList from '../MovieList/MovieList';
 import Header from '../Header/Header';
-import movieData from '../mockData';
+// import movieData from '../mockData';
 import './App.css';
 import MovieDetail from '../MovieDetail/MovieDetail';
+import { getAllMovies, getMovieDetails } from '../api-calls/api-calls';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      movies: movieData.movies,
-      selectedMovie: null
+      movies: [],
+      selectedMovie: null,
+      error:''
     }
   }
 
-  handleClick = (id) => {
-    const clickedMovie = this.state.movies.find(movie => movie.id === id);
-    this.setState({ selectedMovie: clickedMovie });
+  componentDidMount() {
+    getAllMovies()
+    .then(data => {
+      this.setState({ movies: data.movies })
+    })
+    .catch(() => this.setState(
+      { error: 'There was an issue connnecting to the server. Please try again later' }))
   }
 
   goBacktoMainView = () => {
     this.setState({ selectedMovie: null });
   }
 
+
+  handleClick = (id) => {
+    const clickedMovie = this.state.movies.find(movie => movie.id === id);
+    this.setState({ selectedMovie: clickedMovie });
+  }
+
+
   render() {
     return (
       <div className="container">
       <main className='main'>
+        {this.state.error &&
+          <h1>{this.state.error}</h1>}
         {!this.state.selectedMovie && <>
           <Header />
           <MovieList movies={ this.state.movies } handleClick={this.handleClick}/>
