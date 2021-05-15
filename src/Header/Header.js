d468eeadab67b81e77730837aa08fba95f2cca40
kendallha <kendallhaw@gmail.com>
2021-05-14 19:33:58 -0700

    Add linear gradient background and add midpoint grid layout for MovieList

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -4,7 +4,7 @@ import './Header.css';
 const Header = () => {
   return (
     <header className='header'>
-      <h1 className='app-name'>Rancid Tomatillos</h1>
+      <h1 className='app-name'>rancid tomatillos</h1>
       <h2 className='page-title'>Popular Movies this Month</h2>
     </header>
   )
