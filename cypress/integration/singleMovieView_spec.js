c8af0a17a1e0e016cef4e5958089507e6684cab5
kendallha <kendallhaw@gmail.com>
2021-05-18 16:00:53 -0700

    Add test for clicking button to return to main page

--- cypress/integration/singleMovieView_spec.js
+++ cypress/integration/singleMovieView_spec.js
@@ -4,10 +4,7 @@ describe('AllMovieView', () => {
       cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies', { body: movies }).as('movies')
     })
     cy.visit('http://localhost:3000/')
-  })
-
-  it('should display all of the information for a single movie', () => {
-      cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/1', { 
+     cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/1', { 
         movie: {
         id: 1, 
         title: "Fakest Movie Title", 
@@ -23,9 +20,12 @@ describe('AllMovieView', () => {
         tagline: "It's a movie!"
         }
       }) 
-       
+
     cy.get('#1').click();
-      cy.get('.movie-title').contains('Fakest Movie Title')
+  })
+
+  it('should display all of the information for a single movie', () => {
+    cy.get('.movie-title').contains('Fakest Movie Title')
       .contains('2019')
       .get('.overview').contains('Some overview that is full of buzzwords')
       .get('.average-rating').contains('6')
@@ -35,5 +35,17 @@ describe('AllMovieView', () => {
       .get('.movie-details-aside').contains('100,853,753')
       .get('.tagline').contains('It\'s a movie!')
       .get('.poster-wrapper').find('img')
+  })
+
+  it('should not display the other movies when one is selected', () => {
+    cy.get('.movie').should('not.exist');
+  })
+
+  it('should go back to showing all movies when the back button is clicked', () => {
+    cy.get('i').click()
+      .get('.app-name').contains('Rancid Tomatillos')
+      .get('.page-title').contains('Popular Movies this Month')
+      .get('.movie-list').children().should('have.length', 41)
+      .get('.movie-expanded').should('not.exist');
   })
 })
