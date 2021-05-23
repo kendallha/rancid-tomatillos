87c965a5542500d269f9b132cf0211b4502a81dd
kendallha <kendallhaw@gmail.com>
2021-05-23 13:03:47 -0700

    Update cypress SearchBar test to use button instead of i

--- cypress/integration/searchBar_spec.js
+++ cypress/integration/searchBar_spec.js
@@ -8,18 +8,18 @@ describe('searchBar', () => {
 
   it('should have a search input and button on the page', () => {
     cy.get('form').find('input')
-      .get('form').find('i')
+      .get('form').find('button')
   })
 
   it('should be able to search the movie titles that contain what is in the search input', () => {
     cy.get('input').type('Money')
-      .get('i').click()
+      .get('button').click()
       .get('.movie-list').children().should('have.length', 1)
       .get('.title').contains('Money')
   })
 
   it('should be able to hit search with nothing in the input and have it display all the movies', () => {
-    cy.get('i').click()
+    cy.get('button').click()
       .location('pathname').should('eq', '/')
       .get('.movie-list').children().should('have.length', 41)
       .get('.page-title').contains('Popular Movies this Month')
@@ -27,16 +27,16 @@ describe('searchBar', () => {
 
   it('should be able to hit search with nothing in the input and have it display all the movies, after searching for a movie', () => {
     cy.get('input').type('Money')
-      .get('i').click()
+      .get('button').click()
       .get('input').clear()
-      .get('i').click()
+      .get('button').click()
       .get('.movie-list').children().should('have.length', 41)
       .get('.page-title').contains('Popular Movies this Month')
   })
 
   it('should be able to return to the main page by clicking the title, after searching for a movie', () => {
     cy.get('input').type('Money')
-      .get('i').click()
+      .get('button').click()
       .get('.app-name').click()
       .location('pathname').should('eq', '/')
       .get('.movie-list').children().should('have.length', 41)
@@ -46,19 +46,19 @@ describe('searchBar', () => {
   it('should take the user to a page with it\'s own url when the user searches', () => {
     cy.location('pathname').should('eq', '/')
       .get('input').type('Money')
-      .get('i').click()
+      .get('button').click()
       .location('pathname').should('eq', '/search/Money')
   })
 
   it('should show the user what they had typed as a search, on the search results page', () => {
     cy.get('input').type('Money')
-      .get('i').click()
+      .get('button').click()
       .get('.page-title').contains('Search results for "Money"')
   })
 
   it ('should inform the user if there aren\'nt any titles that match their search input', () => {
     cy.get('input').type('Moneyes')
-      .get('i').click()
+      .get('button').click()
       .get('.no-results').contains('There are no movie titles matching your search')
   })
 })
